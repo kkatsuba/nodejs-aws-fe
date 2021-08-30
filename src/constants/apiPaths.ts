2e3f9c7238da5fb44f3e48466d34984e9546bde2
Kiryl Katsuba <kiryl.katsuba@logmein.com>
2021-08-30 17:06:06 +0300

    update import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 export const API_PATHS = {
   product: 'https://860y5xq1c2.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://5wvj4wnd9h.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
